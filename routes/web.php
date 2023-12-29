1e1e3d19153baab40e746334387c65c61b629fa5
RizkyPM40076 <rizky.213040097@mail.unpas.ac.id>
2023-12-29 16:26:57 +0700

    penambahan untuk catagory

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,3 +30,5 @@ Route::middleware([
 });
 
 Route::get('/redirect', [HomeController::class, 'redirect']);
+
+Route::get('/view_catagory', [AdminController::class, 'view_catagory']);
