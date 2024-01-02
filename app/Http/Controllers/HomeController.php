b94fbc1b30099ca2190bb992b9c0bb04ed869887
RizkyPM40076 <rizky.213040097@mail.unpas.ac.id>
2024-01-02 22:29:14 +0700

    memperbaiki halaman userpage, dan menambah halaman order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -257,5 +257,37 @@ class HomeController extends Controller
         return back();
     }
 
+    public function show_order()
+    {
+        if(Auth::id())
+        {
+
+            $user=Auth::user();
+
+            $userid=$user->id;
+
+            $order=order::where('user_id','=',$userid)->get();
+            return view('home.order', compact('order'));
+        }
+
+        else
+        {
+            return redirect('login');
+        }
+
+    }
+
+    public function cancel_order($id)
+    {
+        $order=order::find($id);
+
+        $order->delivery_status='Sucessfully Canceled';
+
+        $order->save();
+
+        return redirect()->back();
+
+    }
+
 
 }
